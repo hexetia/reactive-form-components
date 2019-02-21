86bfb484ff9ea2798f18f2d45a1513702db7c59e
Lucas <lucasljj@gmail.com>
2019-02-21 13:03:31 -0300

    add index.ts to export modules

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {AutoCompleteReactive} from "./autoComplete/autoCompleteReactive";
+export {TextInput} from './textInput/TextInput';
+export {SwitchInput} from './switchInput/SwitchInput';
+export {CheckboxInput} from './checkboxInput/CheckboxInput';
