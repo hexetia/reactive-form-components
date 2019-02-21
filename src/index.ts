7e7d8d32aba6d4c3becfacb2ddb8407d6aacd566
Lucas <lucasljj@gmail.com>
2019-02-21 17:34:42 -0300

    move away from src folder

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export {AutoCompleteReactive} from "./autoComplete/autoCompleteReactive";
-export {TextInput} from './textInput/TextInput';
-export {SwitchInput} from './switchInput/SwitchInput';
-export {CheckboxInput} from './checkboxInput/CheckboxInput';
